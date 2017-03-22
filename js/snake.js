f031e98ec6e32a53805e99183b4d589f611f3a25
Ben Walley <benwalleyorigami@gmail.com>
2017-03-22 10:44:45 -0500

    made it so that it covered the whole page, and used absolute positioning

--- js/snake.js
+++ js/snake.js
@@ -1,8 +1,15 @@
 var square = $(".square");
+var main = $("#main")
+	var width = window.screen.availWidth
+	var height = window.screen.availHeight
+	console.log(width, height)
+	cols = Math.floor(width/50)
+	rows = Math.floor(height/50)
 
-var snake = [10,11,12,13,14,24,34,44,54]
+
+var snake = [0,1,2,3,4,5,6,7]
 // 1 is right, -1 is left, 10 is down, and -10 is up
-var direction = 10
+var direction = 1
 var counter = 0
 
 // CLICK LISTENERS
@@ -14,11 +21,11 @@ function checkKey(e) {
 
     if (e.keyCode == '38') {
         // up arrow
-        direction = -10
+        direction = -cols
     }
     else if (e.keyCode == '40') {
         // down arrow
-        direction = 10
+        direction = cols
     }
     else if (e.keyCode == '37') {
        // left arrow
@@ -31,10 +38,35 @@ function checkKey(e) {
 
 }
 
+function init(){
+
+	console.log(rows, cols)
+	var widthPointer = 0
+	var heightPointer = 0
+	var counter = 0
+	for (var i = 0; i < rows; i++) {
+		for (var g = 0; g < cols; g++) {
+			console.log("loop is running")
+			var node = document.createElement("div");               
+			document.body.appendChild(node);  
+			node.className += "square"
+			var square = $(".square")
+			square[counter].style.top = (i*50) + "px"
+			square[counter].style.left = (g*50) + "px"
+			counter++
+		};
+	};
+	
+}
+
+init()
+
+
+var square = $(".square");
 
 function renderSnake(){
 	for (var i = 0; i < square.length; i++) {
-		square[i].style.background = "white"
+		square[i].style.background = "rgba(0,0,0,0)"
 	};
 	for (var i = 0; i < snake.length; i++) {
 		square[snake[i]].style.background = "blue"
